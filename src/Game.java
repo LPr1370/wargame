0d47e0948f0b71cccde2fa0be499b0363e59ddc9
Churina <113448247+Churina@users.noreply.github.com>
2022-12-09 20:22:18 -0500

    renamed Deck to Game

--- src/Game.java
+++ src/Game.java
@@ -6,7 +6,7 @@
  */
 
 import java.util.ArrayList;
-public class Deck {
+public class Game {
     public static void main(String[] args) {
         //this is making the suits and values for the cards
         String[] suits = {"Hearts", "Spades", "Clubs", "Diamonds"};
